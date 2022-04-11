ec360e7672ef0c30826321391181d87bdeb57bc7
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-04-11 20:28:25 -0300

    fiz o exercicio 7 parte 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-parte-1/exercicios-parte-2/exercicio7.js
@@ -0,0 +1,9 @@
+function recebeStrings (word, ending){
+    if(word.endsWith(ending)){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+}
+
+recebeStrings("trybe", "be")
