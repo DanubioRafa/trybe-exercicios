48a8b791d10a1368fd6df67b069ac0de18cc0930
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-04-18 19:12:18 -0300

    Fiz o exercicio 2 e 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercicio/script.js
@@ -20,14 +20,47 @@ const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
 for (index of dezDaysList) {
   let elementLi = document.createElement("li");
   let elementUl = document.getElementById("days");
-  
+
   if (index == 24 || index == 25 || index == 31) {
     elementLi.classList = "day holiday";
-  } else if (index == 4 || index == 11 || index == 18 || index == 25){
+  } else if (index == 4 || index == 11 || index == 18 || index == 25) {
     elementLi.classList = "day friday";
   } else {
     elementLi.className = "day";
   }
   elementLi.innerText = index;
+  elementLi.style.backgroundColor = "rgb(238,238,238)";
   elementUl.appendChild(elementLi);
+  console.log((elementLi.style.backgroundColor));
 }
+
+function createButtonHoli(Feriados) {
+  let elementButton = document.createElement("button");
+  let elementButtonsContainer = document.getElementsByClassName("buttons-container")[0];
+  elementButton.id = "btn-holiday";
+  elementButton.innerText = "Feriados";
+  elementButtonsContainer.appendChild(elementButton);
+
+  elementButton.addEventListener("click", showHolidays);
+
+  let active = false;
+
+  function showHolidays() {
+    let holidaysList = document.querySelectorAll(".holiday");
+
+    if (active === false) {
+      active = true;
+    } else {
+      active = false;
+    }
+
+    for (let index = 0; index < holidaysList.length; index += 1) {
+      if (active === true) {
+        holidaysList[index].style.backgroundColor = "black";
+      } else {
+        holidaysList[index].style.backgroundColor = "rgb(238,238,238)";
+      }
+    }
+  }
+}
+createButtonHoli();
